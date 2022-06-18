395c2a53e28c4c951233127bbd8a5cbd79603425
Eugen Rudic <e.rudic@gmail.com>
2022-06-18 20:22:14 +0200

    fix: data downlad script path and unzip

--- scripts/download.py
+++ scripts/download.py
@@ -1,5 +1,5 @@
 from kaggle import api as kaggle_api
 
 dataset = "sobhanmoosavi/us-accidents"
-outpath = "../data/us-accidents.zip"
-kaggle_api.dataset_download_files(dataset,outpath)
+outpath = "data/"
+kaggle_api.dataset_download_files(dataset,outpath,unzip=True)

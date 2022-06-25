c42a3620750a7a1d23bd57bbf7e2d1efcbf525da
Eugen Rudic <e.rudic@gmail.com>
2022-06-25 11:03:18 +0200

    fix: no num param

--- model_trainer/trainers/xgboost_trainer.py
+++ model_trainer/trainers/xgboost_trainer.py
@@ -5,7 +5,6 @@ import xgboost
 
 class XGBoostTrainer(BaseTrainer):
     def _train_model(self,params,X_train,y_train,X_val,Y_val):
-        num_round = params.pop('num_round')
         model = xgboost.XGBClassifier(**params,verbosity=2)
         
         classes_weights = class_weight.compute_sample_weight(
@@ -13,7 +12,7 @@ class XGBoostTrainer(BaseTrainer):
             y=y_train
         )
 
-        model.fit(X_train, y_train, num_round=num_round, sample_weight=classes_weights)
+        model.fit(X_train, y_train, sample_weight=classes_weights)
         return model
     
     def _validate_model(self,model,X_val=None,Y_val=None):

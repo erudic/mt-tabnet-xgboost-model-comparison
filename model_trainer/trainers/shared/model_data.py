3873bcb4d7ccacd563ebb94fe88d6d4ccb97e0bf
Eugen Rudic <e.rudic@gmail.com>
2022-08-07 17:26:21 +0200

    fix: delete old trainers

--- model_trainer/trainers/shared/model_data.py
+++
@@ -1,6 +0,0 @@
-class ModelData:
-    def __init__(self,X_train,Y_train,X_val,Y_val):
-        self.X_train=X_train,
-        self.Y_train=Y_train,
-        self.X_val=X_val,
-        self.Y_val=Y_val

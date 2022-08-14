d3fb52c8b8be6fb8270d0ca5e1de28eab37e3ac4
Eugen Rudic <e.rudic@gmail.com>
2022-08-14 20:52:13 +0200

    feat: add sample weights for validation xgboost

--- model_trainer/xgboost/xgboost_trainer.py
+++ model_trainer/xgboost/xgboost_trainer.py
@@ -10,28 +10,35 @@ import xgboost
 class XGBoostTrainer:
     def __init__(self, model_params: Dict, class_weights: Union[List[float], None] = None):
         self.model_params = model_params
-        self.class_weights = class_weights
+        self.class_weights = class_weights if class_weights != None else 'balanced'
 
-    def train(self, X_train, Y_train, X_val,Y_val, verbosity=1):
+    def train(self, X_train, Y_train, X_val, Y_val, verbosity=1):
         model = xgboost.XGBClassifier(
-            **self.model_params, 
+            **self.model_params,
             verbosity=verbosity
         )
 
-        class_weights = self.class_weights if self.class_weights!=None else 'balanced'
+        sample_weights_val = self.compute_sample_weights(Y_val)
+        sample_weights_train = self.compute_sample_weights(Y_train)
 
-        sample_weights = class_weight.compute_sample_weight(
-            class_weight=class_weights,
-            y=Y_train
-        )
+        model.fit(X_train, Y_train, eval_set=[(X_val, Y_val)],
+                  sample_weight=sample_weights_train,
+                  sample_weight_eval_set=sample_weights_val
+                  )
 
-        model.fit(X_train, Y_train, eval_set=[(X_val,Y_val)], verbose_eval=10, sample_weight=sample_weights)
         return model
-    
-    def train_and_validate(self,X_train, Y_train, X_val, Y_val, verbosity=1):
-        model = self.train(X_train, Y_train, X_val, Y_val,verbosity)
-        metric = self.validate(model,X_val,Y_val)
+
+    def train_and_validate(self, X_train, Y_train, X_val, Y_val, verbosity=1):
+        model = self.train(X_train, Y_train, X_val, Y_val, verbosity)
+        metric = self.validate(model, X_val, Y_val)
         return model, metric
 
-    def validate(self,model, X_val, Y_val):
-        return matthews_corrcoef(Y_val,model.predict(X_val))
+    def validate(self, model, X_val, Y_val):
+        sample_weights = self.compute_sample_weights(Y_val)
+        return matthews_corrcoef(Y_val, model.predict(X_val), sample_weight=sample_weights)
+
+    def compute_sample_weights(self, Y):
+        return class_weight.compute_sample_weight(
+            class_weight=self.class_weights,
+            y=Y
+        )

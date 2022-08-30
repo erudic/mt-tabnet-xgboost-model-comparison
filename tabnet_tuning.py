fix: except cuda error - possible gradient problem

tabnet_tuning.py
@@ -44,8 +44,16 @@ def tabnet_fn(params,databox, callbacks,epochs):
     for X_train, Y_train, X_val, Y_val in databox.get_processed_data():
         tt = TabNetTrainer(lr,model_params, optimizer,
                            batch_size, callbacks, class_weights)
-        model, metric = tt.train_and_validate(X_train, Y_train, X_val, Y_val, data_config.continous_variables,epochs)
-        metrics.append(metric)
+        try:
+            model, metric = tt.train_and_validate(X_train, Y_train, X_val, Y_val, data_config.continous_variables,epochs)
+            metrics.append(metric)
+        except RuntimeError as e:
+            if "CUDA ERROR:" in str(e):
+                print("Error while trainin:" + e.with_traceback)
+                print("Setting metric to -1")
+                metrics.append(-1.0)
+            else:
+                raise e
     print(f"Training finished got metrics: {metrics}")
     return -np.average(metrics)
 